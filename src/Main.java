8fdaf53343b35b602faa69e09e6ac3ecc296c0b4
priscilaeroner <119367886+priscilaeroner@users.noreply.github.com>
2022-12-08 21:30:11 -0300

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.time.LocalDate;
+public class Main {
+    public static void main(String[] args) {
+
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Descricao Curso Java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso Javascript");
+        curso2.setDescricao("Descricao Curso Javascript");
+        curso2.setCargaHoraria(8);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria de Java");
+        mentoria.setDescricao("Descricao Mentoria Java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+    }
+}
---
+++ src/Mentoria.java
@@ -0,0 +1,43 @@
+import java.time.LocalDate;
+
+public class Mentoria {
+    private String titulo;
+    private String descricao;
+    private LocalDate data;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void main(String[] args) {
+        LocalDate data = LocalDate.now();
+    }
+
+    @Override
+    public String toString() {
+        return "Curso{" +
+                "titulo='" + titulo + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", data=" + data +
+                '}';
+
+    }
+
+
+}
